f603496546c9e1b61f0259bf492a6501766a04f3
Jose Castro <gabriel_castro89@hotmail.com>
2019-11-13 11:14:08 +0000

    new work tree

---
+++ public/client.js
@@ -0,0 +1,242 @@
+//importa os outros ficheiros js
+// var scripts = ["/static/three.js","/static/Mat/frac.js","/static/Mat/geom.js","/static/Mat/area.js","/static/Mat/perim.js","/static/Mat/maior_menor.js",
+// "/static/Mat/div_com.js","/static/Mat/vol.js","/static/Mat/pot.js","/static/Mat/frac_aprox_unidade.js","/static/Mat/mult_n_racio.js","/static/Mat/pot_frac.js",
+// "/static/Mat/arredonda.js","/static/Mat/pot_pot.js","/static/Mat/raizes.js","/static/Mat/graf.js","/static/Mat/reta_num.js","/static/Mat/frac_sinal.js",
+// "/static/Mat/func_graf.js","/static/Mat/func_graf2.js"];
+
+// for (var i = 0; i < scripts.length; i++) {
+//   var imported = document.createElement("script");
+//   imported.src = scripts[i];
+//   document.getElementsByTagName("head")[0].appendChild(imported);
+// }
+
+//variaveis
+var funcs = new Array(10);
+for (var i = 0; i < funcs.length; i++) {
+  funcs[i] = new Array(10);
+  // for (var j = 0; j < funcs[i].length; j++) {
+  //   funcs[i][j] = new Array(10);
+  // }
+}
+
+window.onload = function() {
+  buildUserInput();
+};
+
+//const frac = require('./frac');
+const materias = [
+  ["Frações", "Areas", "Perimetros", "Divisores comuns"],
+  ["Volumes","Potências","Fração VS Unidade","Área colorida(Frações)","Potências(Frações)","Arredondamentos"],
+  ["Potência de Potência","Raízes","Reta númerica","Frações com sinal","Maior ou Menor","Grafico 1","Grafico 2"]
+];
+
+var jogador = {lobbyID: 0, nome : "", num: 0, ano: 0, pontos: 0, pronto: 0};
+var submete = 0;
+var pagina = 0;
+var modo = "erro";
+var cor_certo = "#88ff91";
+var cor_errado = "#ff8888";
+
+function buildUserInput(){
+  var str="<input type='text' id='userName' name='userName' value=''><br>";
+  str+="<select id='userYear'>";
+  str+="<option value='5'>5</option>";
+  str+="<option value='6'>6</option>";
+  str+="<option value='7'>7</option>";
+  str+="</select><br>";
+  str+="<button onclick='getUserInput()' class='dropbtn' id='bgetUserInput'>Login</button>";
+  document.getElementById("user_input").innerHTML = str;
+}
+
+function buildQuiz(){
+  var str = '<div><h1 id="tituloMateria"></h1>';
+  str += '<p class="cabecalho_pag" id="cabecalho_pag">Pagina: 1</p></div>';
+  str += '<p></p><button onclick="pag_ant()" class="dropbtn" id="banterior">Anterior</button>&nbsp;';
+  str += '<button onclick="pag_seg()" class="dropbtn" id="bseguinte">Seguinte</button><p></p>';
+  str += '<div class"quizzes" id="quizzesMain"></div>';
+  str += '<div><p></p><button onclick="sairQuiz()" class="dropbtn" id="bsairQuiz">Sair</button>';
+  str += '<button onclick="verificar()" class="dropbtn" id="bverificar">Submeter</button></div>';
+  document.getElementById("quizzes").innerHTML=str;
+}
+
+function buildChatRoom(){
+  document.getElementById("quizzes").innerHTML="A espera que todos os jogadores terminem... ... ...";
+}
+
+function buildLobbyRoom(){
+  document.getElementById("quizzes").innerHTML="A espera que todos os jogadores entrem... ... ...";
+}
+
+function buildLocalGlobal(){
+  str = '<button onclick="local()" class="dropbtn" id="blocal">Local</button>';
+  str += '<button onclick="global()" class="dropbtn" id="bglobal">Global</button>';
+  document.getElementById("local_global").innerHTML=str;
+}
+
+function hide(eid){
+  document.getElementById(eid).style.display = 'none';
+}
+
+function show(eid){
+  document.getElementById(eid).style.display = 'inline-block';
+}
+
+function local() {
+  hide("local_global");
+  var str="<button onclick='solo()' class='dropbtn' id='bsolo'>Solo</button>";
+  str+="<button onclick='mm1v1()' class='dropbtn' id='blobby'>Lobby</button>";
+  document.getElementById("solo_lobby").innerHTML = str;
+}
+
+function global() {
+  hide("local_global");
+  var str="<button onclick='casual()' class='dropbtn' id='bcasual'>Casual</button>";
+  str+="<button onclick='rank()' class='dropbtn' id='brank'>Rank</button>";
+  document.getElementById("casual_rank").innerHTML = str;
+}
+
+function solo() {
+  modo = "solo";
+  hide("solo_lobby");
+  var str="<button onclick='mat()' class='dropbtn' id='bmat'>Matemática</button>";
+  str+="<button onclick='cien()' class='dropbtn' id='bcien'>Ciências</button>";
+  str+="<button onclick='geo()' class='dropbtn' id='bgeo'>Geografia</button>";
+  document.getElementById("disciplina").innerHTML = str;
+}
+
+
+function mm1v1() {
+  modo = "1v1";
+  hide("solo_lobby");
+  pagina = 0;
+  hide("materia");
+  enter_lobby();
+  titulo(jogador.ano,0);
+  return false;
+}
+
+function mat(){
+  hide("disciplina");
+  show("materia");
+  materia(jogador.ano-5,materias[jogador.ano-5]);
+}
+
+function casual() {
+  hide("local_global");
+  return false;
+}
+
+function rank() {
+  hide("local_global");
+  return false;
+}
+
+function getUserInput(){
+  var e = document.getElementById("userYear");
+  jogador.nome = document.getElementById("userName").value
+  jogador.ano = e.options[e.selectedIndex].value;
+  hide("user_input");
+  buildLocalGlobal();
+}
+
+function titulo(ano,i){
+  if (modo=="solo"){
+    document.getElementById("tituloMateria").innerHTML =(ano+5) + "º ano - Materia: " + materias[ano][i];
+  }else if (modo=="1v1"){
+    document.getElementById("tituloMateria").innerHTML =(ano) + "º ano - Multiplayer: 1 V 1";
+  }
+}
+
+function criaHTML(element,className,id,inner,click){
+  var html = document.createElement(element);
+  if(className!=null)html.className=className;
+  if(id!=null)html.id=id;
+  if(inner!=null)html.innerHTML = inner;
+  if(click!=null)html.addEventListener("click", click);
+  return html;
+}
+
+function materia(ano,materia){
+  document.getElementById("materia").innerHTML="";
+  for (var i = 0; i < materia.length; i++) {
+    if((typeof materia[i])=="string"){
+      var novo_botao = criaHTML("button","dropbtn","b"+materia[i],materia[i],getPage);
+      document.getElementById("materia").appendChild(novo_botao);
+    }
+  }
+  var nova_div = criaHTML("div",null,"div_muda_ano",null,null);
+  document.getElementById("materia").appendChild(nova_div);
+}
+
+async function getPage(){
+  buildQuiz();
+  const options = {
+    method: 'POST',
+    headers:{'Content-Type':'application/json'},
+    body: JSON.stringify({})
+  };
+  const res = await fetch('/getPage', options);
+  const data = await res.json();
+  hide("materia");
+  document.getElementById("quizzesMain").innerHTML=data.quizPage;
+  for(var i = 0; i<10;i++){
+      document.getElementById('img'+i).src = data.canvas[i];
+  }
+  document.getElementById("banterior").disabled = true;
+  hide("bverificar");
+}
+
+function pag_ant(){
+  if (pagina>0){
+    hide("pag"+pagina);
+    pagina--;
+    show("pag"+pagina);
+    document.getElementById("cabecalho_pag").innerHTML = "Pagina: " + (pagina + 1);
+    document.getElementById("bseguinte").disabled = false;
+    if (pagina==0){
+      document.getElementById("banterior").disabled = true;
+    }
+  }
+}
+
+function pag_seg(){
+  if (pagina<9){
+    hide("pag"+pagina);
+    pagina++;
+    show("pag"+pagina);
+    document.getElementById("cabecalho_pag").innerHTML = "Pagina: " + (pagina + 1);
+    document.getElementById("banterior").disabled = false;
+    if (pagina==9){
+      show("bverificar");
+      document.getElementById("bseguinte").disabled = true;
+    }
+  }
+}
+
+async function verificar(){
+  var sol = new Array(10);
+  for (var i = 0; i < 10; i++){
+    var radios = document.getElementsByName('solucao'+i);
+    for (var j = 0; j < radios.length; j++){
+      if (radios[j].checked) {
+        sol[i] = document.getElementById("label"+radios[j].value+i).innerHTML;
+        break;
+      }
+    }
+  }
+  const options = {
+    method: 'POST',
+    headers:{'Content-Type':'application/json'},
+    body: JSON.stringify({solution:sol})
+  };
+  const res = await fetch('/verificar', options);
+  const data = await res.json();
+  alert(data.score);
+}
+
+function sairQuiz(){
+  buildQuiz();
+  hide("quizzes");
+  show("solo_lobby");
+  pagina = 0;
+}
--- server.js
+++ server.js
@@ -1,13 +1,56 @@
+
+class lobby {
+	constructor(id, waiting, ano, jogadores, maxJogadores, data) {
+		this._id = id;
+		this._waiting = waiting;
+		this._ano = ano;
+		this._jogadores =[jogadores];
+		this._maxJogadores = maxJogadores;
+		this._data = data;
+	}
+	set waiting(w){
+		this._waiting=w;
+	}
+	get waiting(){
+		return this._waiting;
+	}
+	set ano(a){
+		this._ano=a;
+	}
+	get ano(){
+		return this._ano;
+	}
+	set maxJogadores(m){
+		this._maxJogadores = m;
+	}
+	get maxJogadores(){
+		return this._maxJogadores;
+	}
+	set jogadores(j){
+		this._jogadores.push(j);
+	}
+	get jogadores(){
+		return this._jogadores;
+	}
+	set id(i){
+		this._id = i;
+	}
+	get id(){
+		return this._id;
+	}
+}
+
+const MAX_LOBBYS = 100;
 const path = require('path');
 const host = '0.0.0.0';
 const port = 3000;
 const express = require('express');
 const app = express();
-const Chance = require('chance');
-const chance = new Chance();
-const crypto = require('crypto');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const frac = require('./Mat/5/frac');
+const area = require('./Mat/5/area');
+const script = require('./script');
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -17,52 +60,128 @@ app.listen(port, host, function() {//poe o sv a correr
 	console.log('Running at: ',host,port);
 });
 
-app.use('/static', express.static('public'));
-//app.use(express.json({limit:'1mb'}));
+app.use(express.static('public'));
+app.use(express.json({limit:'1mb'}));
 
-app.get('/', function(req, res){
-    res.sendFile(path.join(__dirname,'/index.html'));
+var lobbys = new Array(MAX_LOBBYS);
+var solutions = new Array(10);
+
+app.post('/verificar',(req, res) => {
+	var sol = req.body.solution;
+	var pontos = 0;
+	for(var i = 0; i<10; i++){
+		if(solutions[i]===sol[i]) pontos++;
+	}
+	res.send({score:pontos});
 });
 
-var nJg=0;
-var objSol;
-var score1=0;
-var score2=0;
+app.post('/getPage',(req, res) => {
+	var quizPage ="";
+	var canvas= new Array(10);;
+	for(var i = 0; i<10; i++){
+		const data = area.f(i);
+		solutions[i] = data.solution;
+		canvas[i] = data.canvas;
+		quizPage+=script.buildPage(i, data.q, data.s1, data.s2, data.s3, data.s4);
+	}
+	res.send({quizPage:quizPage, canvas:canvas});
+});
 
 app.post('/waiting',(req, res) => {
-    if(nJg===2) res.send({status:"OK"});
-    else res.send({status:"NO"});
+	var cont=0;
+	var prontos=0;
+	while(lobbys[cont].id!=req.body.user.lobbyID){
+		cont++;
+	}
+	const nUsers =lobbys[cont].jogadores.length;
+	const user = lobbys[cont].jogadores;
+	if(req.body.type=="ENTER"){
+		if(nUsers==lobbys[cont].maxJogadores){
+			res.send({status:"YES"});
+		}else {
+			res.send({status:"NO"});
+		}
+	}else{
+		for(var i = 0; i<nUsers; i++){
+			if(user[i].pronto == 1) prontos++;
+		}
+		if(prontos==lobbys[cont].maxJogadores){
+			res.send({status:"YES"});
+		}else {
+			res.send({status:"NO"});
+		}
+	}
 });
 
 app.post('/1v1score',(req, res) => {
-    console.log(req.body.player);
-    if(req.body.player=="Jogador 1") score1=req.body.score;
-    else score2=req.body.score;
-    res.send({ status: 'score save: SUCCESS',score: req.body.score });
+	var cont = 0;
+	var prontos = 0;
+	while(lobbys[cont].id!=req.body.jogador.lobbyID){
+		cont++;
+	}
+	console.log("lobby: ",lobbys[cont]);
+	const nUsers =lobbys[cont].jogadores.length;
+	const user = lobbys[cont].jogadores;
+	for(var i = 0; i<nUsers; i++){
+		if(user[i].num==req.body.jogador.num){
+			lobbys[cont].jogadores[i].pontos = req.body.jogador.pontos;
+			lobbys[cont].jogadores[i].pronto = 1;
+		}
+		if(lobbys[cont].jogadores[i].pronto == 1){
+			prontos++;
+		}
+	}
+	if (prontos==1) res.send({status:"score saved", first:"TRUE"});
+	else res.send({status:"score saved", first:"FALSE"});
+	return;
 });
 
 app.post('/checkScore',(req, res) => {
-    if(score1>score2) res.send({ status: 'Jogador 1 ganhou!!' });
-    else if(score1<score2) res.send({ status: 'Jogador 2 ganhou!!' });
-    else if(score1==score2) res.send({ status: 'Empate' });
-    else res.send({ status: 'ERROR' });
+	var cont=0;
+	while(lobbys[cont].id!=req.body.user.lobbyID){
+		cont++;
+	}
+    res.send({ list:lobbys[cont].jogadores });
 });
 
 app.post('/lobby',(req, res) => {
-  nJg++;
-  if(nJg===1){
-    res.send({ player: '1' });
-  }else{
-    res.send({ player: '2' });
-    nJg=0;
-  }
+	var cont=0;
+	while(lobbys[cont]!=null){
+		console.log("numero jogadores: ", lobbys[cont].jogadores.length," MAX: ",lobbys[cont].maxJogadores);
+		if(lobbys[cont].jogadores.length < lobbys[cont].maxJogadores){
+			if(lobbys[cont].ano===req.body.jogador.ano){
+				var novo_jogador = req.body.jogador;
+				novo_jogador.lobbyID=lobbys[cont].id;
+				novo_jogador.num=lobbys[cont].jogadores.length+1;
+				lobbys[cont].jogadores.push(novo_jogador);
+				res.send({ lobbyID:lobbys[cont].id, player: novo_jogador.num });
+				console.log("Jogador registado no lobby: ",lobbys[cont].id);
+				console.log("lobby: ",lobbys[cont]);
+				return;
+			}
+		}
+		cont++;
+	}
+	var ID = Math.floor(Math.random() * 99999)
+	console.log("Novo lobby com o ID: ",ID);
+	res.send({ lobbyID:ID, player: 1 });
+	var novo_jogador = req.body.jogador;
+	novo_jogador.lobbyID=ID;
+	novo_jogador.num=1;
+	lobbys[cont] = new lobby(ID,0, req.body.jogador.ano, novo_jogador, 2, null);
+	console.log("Jogador entrou no lobby: ",ID);
+	console.log("lobby: ",lobbys[cont]);
 });
 
 app.post('/after_lobby',(req, res) => {
-  if(nJg===1){
-    objSol=req.body;
-    res.send({ status: 'SUCCESS' });
-  }else{
-    res.send(objSol);
-  }
+	var cont=0;
+	while(lobbys[cont].id!=req.body.lobbyID){
+		cont++;
+	}
+	if(req.body.num==1){
+		lobbys[cont].data = {f:req.body.f, data:req.body.data};
+		res.send({ status: 'SUCCESS' });
+	}else{
+		res.send(lobbys[cont].data);
+	}
 });
